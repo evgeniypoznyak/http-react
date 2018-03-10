15237504b0252c5802703d86c50a8ccc019bc56f
Evgeniy Poznyak <evgeniyp@fandiexpress.com>
2018-03-10 10:45:10 -0600

    post request

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,30 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import classes from './NewPost.css';
+import axios from 'axios';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Evgeniy'
     }
 
-    render () {
+    postDataHandler = () => {
+        const post = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author,
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts/', post).then((response) => {
+            console.log(response);
+        })
+    }
+
+
+    render() {
         return (
+
             <div className={classes.NewPost}>
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title}
+                       onChange={(event) => this.setState({title: event.target.value})}/>
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content}
+                          onChange={(event) => this.setState({content: event.target.value})}/>
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Evgeniy">Evgeniy</option>
+                    <option value="Katia">Katia</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
+
+
         );
     }
 }
 
 export default NewPost;
+
+
